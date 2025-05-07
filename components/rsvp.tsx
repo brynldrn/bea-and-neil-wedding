aa3e42c370ebf34e01194fc61bcaba983e25c3a2
Bryan Aldrin Quinalayo <bryandummy01@gmail.com>
2025-05-07 18:41:09 +0800

    fix: RSVP flower setup

--- components/rsvp.tsx
+++ components/rsvp.tsx
@@ -15,6 +15,7 @@ import { z } from "zod"
 import { zodResolver } from '@hookform/resolvers/zod'
 import { cn } from "@/lib/utils"
 import exclamationMark from '@/public/icons/exclamation.svg'
+import { motion } from "motion/react"
 
 export default function Rsvp() {
   const formSchema = z.object({
@@ -68,121 +69,160 @@ export default function Rsvp() {
     <section id="rsvp" className="p-8 bg-brown-4 flex flex-col gap-16 overflow-hidden lg:py-24">
       <h1 className="font-serif text-cream font-light text-[100px] leading-[90%] tracking-[10px] text-center lg:text-[120px] xl:text-[400px] xl:tracking-[60px]">RSVP</h1>
 
+
       {/* details section */}
-      {!isSubmitted && (
-        <div className="block space-y-4 px-12 max-w-[839px] lg:mx-auto relative xl:pl-36 xl:pr-0">
-          <span className="block text-2xl text-off-white font-serif italic">We hope to celebrate with you!</span>
-          <span className="block font-sans text-base text-cream pr-12">
-            Kindly let us know if you plan to attend by filling out the RSVP form below. Please submit your confirmation on or before <strong>JUNE 15, 2025</strong>.
-          </span>
-          {/* form section */}
-          <Form {...form}>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10 items-start justify-start max-w-[470px] lg:w-full mt-10 relative">
-              {/* flowers */}
-              <div className="hidden xl:flex gap-0 absolute -left-[140%] -bottom-[250px]">
-                <div className='aspect-[414/612] h-[612px] w-[214px] lg:w-[414px] relative mt-14'>
-                  <Image src={flowerExtra} alt='flower' fill className='object-fill' />
-                </div>
-                <div className='aspect-[301/533] h-[533px] w-[301px] relative -ml-32'>
-                  <Image src={flowerExtra2} alt='flower' fill className='object-fill' />
-                </div>
-              </div>
+      <div className="block space-y-4 px-12 max-w-[839px] lg:mx-auto relative xl:pl-36 xl:pr-0">
 
-              {/* checkbox area */}
-              <div className='flex flex-col gap-4'>
-                <p className="font-sans text-base text-cream font-bold">Are you attending the event? <span className="text-brown-6">*</span></p>
-                <FormField name="attending" render={() => (
-                  <FormItem>
-                    <FormControl>
-                      <RadioGroup value={isAttending} className="flex items-center gap-8 justify-start" onValueChange={(value) => setIsAttending(value as 'yes' | 'no')}>
-                        <div className="flex items-center gap-4">
-                          <RadioGroupItem value="yes" id="yes" />
-                          <Label htmlFor="yes" className="text-cream text-base font-light font-sans">Yes</Label>
-                        </div>
-                        <div className="flex items-center gap-4">
-                          <RadioGroupItem value="no" id="no" />
-                          <Label htmlFor="no" className="text-cream text-base font-light font-sans">No</Label>
-                        </div>
-                      </RadioGroup>
-                    </FormControl>
-                  </FormItem>
-                )} />
-              </div>
+        {/* flowers */}
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 200
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          className="hidden xl:flex flex-col gap-0 absolute -left-[500px] top-0"
+        >
+          <div className="flex gap-0">
+            <div className='aspect-[414/612] h-[612px] w-[214px] lg:w-[414px] relative mt-14'>
+              <Image src={flowerExtra} alt='flower' fill className='object-fill' />
+            </div>
+            <div className='aspect-[301/533] h-[533px] w-[301px] relative -ml-32'>
+              <Image src={flowerExtra2} alt='flower' fill className='object-fill' />
+            </div>
+          </div>
+          <div className="flex gap-0 -mt-40">
+            <div className='aspect-[414/612] h-[612px] w-[214px] lg:w-[414px] relative mt-14'>
+              <Image src={flowerExtra} alt='flower' fill className='object-fill' />
+            </div>
+            <div className='aspect-[301/533] h-[533px] w-[301px] relative -ml-32'>
+              <Image src={flowerExtra2} alt='flower' fill className='object-fill' />
+            </div>
+          </div>
+          <div className="flex gap-0 -mt-40">
+            <div className='aspect-[414/612] h-[612px] w-[214px] lg:w-[414px] relative mt-14'>
+              <Image src={flowerExtra} alt='flower' fill className='object-fill' />
+            </div>
+            <div className='aspect-[301/533] h-[533px] w-[301px] relative -ml-32'>
+              <Image src={flowerExtra2} alt='flower' fill className='object-fill' />
+            </div>
+          </div>
+          <div className="flex gap-0 -mt-40">
+            <div className='aspect-[414/612] h-[612px] w-[214px] lg:w-[414px] relative mt-14'>
+              <Image src={flowerExtra} alt='flower' fill className='object-fill' />
+            </div>
+            <div className='aspect-[301/533] h-[533px] w-[301px] relative -ml-32'>
+              <Image src={flowerExtra2} alt='flower' fill className='object-fill' />
+            </div>
+          </div>
+        </motion.div>
 
-              {/* names area */}
-              <div className='flex flex-col gap-4 w-full'>
-                <p className="font-sans text-base text-cream font-bold">{isAttending === 'yes' ? 'Enter your name and the names of those attending with you.' : 'Wish you could be there! Please enter the names of those who will not be attending the event.'} <span className="text-brown-6">*</span></p>
-                {fields?.map((field, index) => {
-                  const { message, name } = errors?.names?.[index] || {}
-
-                  return (
-                    <FormField name={`names.${index}.name`} key={field.id} render={() => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="flex items-center relative">
-                            <input {...register(`names.${index}.name`)} type="text" placeholder="Enter yout name here" className={cn('placeholder:font-serif placeholder:italic placeholder:text-base placeholder:opacity-50 placeholder:text-cream border-1 py-2 text-cream font-serif italic w-full px-4 rounded-sm', {
-                              'border-brown-6': name,
-                              'border-brown-3': !name
-                            })} />
-
-                            <div className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center gap-1">
-                              {name && (
-                                <div className="bg-brown-6 rounded-full size-5 flex items-center justify-center relative">
-                                  <Image src={exclamationMark} fill className="object-fill scale-50" alt="warning" />
-                                </div>
-                              )}
-                              {index !== 0 && (
-                                <button className="bg-white rounded-full size-5 text-center flex items-center justify-center" type="button" onClick={() => remove(index)}>
-                                  <X className="size-3" color="brown" strokeWidth={3} />
-                                </button>
-                              )}
-                            </div>
+        {!isSubmitted && (
+          <>
+            <span className="block text-2xl text-off-white font-serif italic">We hope to celebrate with you!</span>
+            <span className="block font-sans text-base text-cream pr-12">
+              Kindly let us know if you plan to attend by filling out the RSVP form below. Please submit your confirmation on or before <strong>JUNE 15, 2025</strong>.
+            </span>
+            {/* form section */}
+            <Form {...form}>
+              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10 items-start justify-start max-w-[470px] lg:w-full mt-10 relative">
+                {/* checkbox area */}
+                <div className='flex flex-col gap-4'>
+                  <p className="font-sans text-base text-cream font-bold">Are you attending the event? <span className="text-brown-6">*</span></p>
+                  <FormField name="attending" render={() => (
+                    <FormItem>
+                      <FormControl>
+                        <RadioGroup value={isAttending} className="flex items-center gap-8 justify-start" onValueChange={(value) => setIsAttending(value as 'yes' | 'no')}>
+                          <div className="flex items-center gap-4">
+                            <RadioGroupItem value="yes" id="yes" />
+                            <Label htmlFor="yes" className="text-cream text-base font-light font-sans">Yes</Label>
                           </div>
-                        </FormControl>
-                        <FormMessage className="text-brown-6">{message}</FormMessage>
-                      </FormItem>
-                    )} />
-                  )
-                })}
-                {fields?.length <= 15 && (
-                  <button type="button" className="inline-flex items-center gap-2 font-sans text-cream mt-2 bg-white/10 self-start rounded-full py-1 px-2.5" onClick={() => append({ name: '' })}>
-                    Add name <PlusIcon />
-                  </button>
-                )}
-              </div>
+                          <div className="flex items-center gap-4">
+                            <RadioGroupItem value="no" id="no" />
+                            <Label htmlFor="no" className="text-cream text-base font-light font-sans">No</Label>
+                          </div>
+                        </RadioGroup>
+                      </FormControl>
+                    </FormItem>
+                  )} />
+                </div>
 
-              <button disabled={isLoading} type="submit" className="uppercase font-sans bg-cream px-6 py-1 rounded-full text-2xl font-bold w-full text-brown-5 lg:py-4 text-center">
-                {isLoading ? <LoaderCircle className="animate-spin mx-auto text-brown-6" size={32} /> : 'Submit'}
-              </button>
-            </form>
-          </Form>
-        </div>
-      )}
+                {/* names area */}
+                <div className='flex flex-col gap-4 w-full'>
+                  <p className="font-sans text-base text-cream font-bold">{isAttending === 'yes' ? 'Enter your name and the names of those attending with you.' : 'Wish you could be there! Please enter the names of those who will not be attending the event.'} <span className="text-brown-6">*</span></p>
+                  {fields?.map((field, index) => {
+                    const { message, name } = errors?.names?.[index] || {}
 
-      {isSubmitted && (
-        <div className="block space-y-4 px-12 max-w-[839px] lg:mx-auto relative xl:pl-36 xl:pr-0">
-          {/* thank you message */}
-          {isAttending === 'yes' ? (
-            <>
-              <span className="block text-2xl text-off-white font-serif italic">Your RSVP is Confirmed! 🎉</span>
-              <span className="block font-sans text-base text-cream pr-12">
-                We&rsquo;re so grateful you&rsquo;ll be joining us on our big day. Can&rsquo;t waitto see you there!
-              </span>
-            </>
-          ) : (
-            <>
-              <span className="block text-2xl text-off-white font-serif italic">Thank you for letting us know.</span>
-              <span className="block font-sans text-base text-cream pr-12">
-                We&rsquo;ll definitely miss you on the big day 😢 Sending hugs and love your way! 💖
-              </span>
-            </>
-          )}
+                    return (
+                      <FormField name={`names.${index}.name`} key={field.id} render={() => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="flex items-center relative">
+                              <input {...register(`names.${index}.name`)} type="text" placeholder="Enter yout name here" className={cn('placeholder:font-serif placeholder:italic placeholder:text-base placeholder:opacity-50 placeholder:text-cream border-1 py-2 text-cream font-serif italic w-full px-4 rounded-sm', {
+                                'border-brown-6': name,
+                                'border-brown-3': !name
+                              })} />
 
-          <a href="#top" className="block uppercase font-sans bg-cream px-6 py-1 rounded-full text-2xl font-bold w-full text-brown-5 lg:py-4 text-center mt-10">
-            Back to top
-          </a>
-        </div>
-      )}
+                              <div className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center gap-1">
+                                {name && (
+                                  <div className="bg-brown-6 rounded-full size-5 flex items-center justify-center relative">
+                                    <Image src={exclamationMark} fill className="object-fill scale-50" alt="warning" />
+                                  </div>
+                                )}
+                                {index !== 0 && (
+                                  <button className="bg-white rounded-full size-5 text-center flex items-center justify-center" type="button" onClick={() => remove(index)}>
+                                    <X className="size-3" color="brown" strokeWidth={3} />
+                                  </button>
+                                )}
+                              </div>
+                            </div>
+                          </FormControl>
+                          <FormMessage className="text-brown-6">{message}</FormMessage>
+                        </FormItem>
+                      )} />
+                    )
+                  })}
+                  {fields?.length <= 15 && (
+                    <button type="button" className="inline-flex items-center gap-2 font-sans text-cream mt-2 bg-white/10 self-start rounded-full py-1 px-2.5" onClick={() => append({ name: '' })}>
+                      Add name <PlusIcon />
+                    </button>
+                  )}
+                </div>
+
+                <button disabled={isLoading} type="submit" className="uppercase font-sans bg-cream px-6 py-1 rounded-full text-2xl font-bold w-full text-brown-5 lg:py-4 text-center">
+                  {isLoading ? <LoaderCircle className="animate-spin mx-auto text-brown-6" size={32} /> : 'Submit'}
+                </button>
+              </form>
+            </Form>
+          </>
+        )}
+
+
+        {isSubmitted && (
+          <>
+            {/* thank you message */}
+            {isAttending === 'yes' ? (
+              <>
+                <span className="block text-2xl text-off-white font-serif italic">Your RSVP is Confirmed! 🎉</span>
+                <span className="block font-sans text-base text-cream pr-12">
+                  We&rsquo;re so grateful you&rsquo;ll be joining us on our big day. Can&rsquo;t waitto see you there!
+                </span>
+              </>
+            ) : (
+              <>
+                <span className="block text-2xl text-off-white font-serif italic">Thank you for letting us know.</span>
+                <span className="block font-sans text-base text-cream pr-12">
+                  We&rsquo;ll definitely miss you on the big day 😢 Sending hugs and love your way! 💖
+                </span>
+              </>
+            )}
+
+            <a href="#top" className="block uppercase font-sans bg-cream px-6 py-1 rounded-full text-2xl font-bold w-full text-brown-5 lg:py-4 text-center mt-10">
+              Back to top
+            </a>
+          </>
+        )}
+      </div>
     </section>
   )
 }
