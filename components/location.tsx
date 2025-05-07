9746ab4531a59ca6b6d63d0ecb4bf7bf8bcc191d
Bryan Aldrin Quinalayo <bryandummy01@gmail.com>
2025-05-07 22:32:04 +0800

    fix: update flowers in location block

--- components/location.tsx
+++ components/location.tsx
@@ -10,7 +10,7 @@ import { SquareArrowOutUpRight } from 'lucide-react';
 
 export default function Location() {
   return (
-    <section id="details" className="mt-16 py-16 pt-[100px] bg-light-green flex flex-col gap-20 items-center justify-center rounded-tl-full rounded-tr-full px-12 relative overflow-hidden max-w-[1440px] xl:mx-auto">
+    <section id="details" className="mt-16 py-16 pt-[100px] bg-light-green flex flex-col gap-20 items-center justify-center rounded-tl-full rounded-tr-full px-12 relative overflow-hidden max-w-[1440px] xl:mx-auto 2xl:overflow-visible">
       <span className="block text-white font-serif italic text-[32px] text-center w-[277px] line-clamp-2 -mt-5 md:px-34 md:w-[730px] md:line-clamp-3 lg:mt-20 lg:w-[953px] lg:text-[56px]">
         Together with hearts full of love, we invite you to be part of our wedding day
       </span>
@@ -49,12 +49,12 @@ export default function Location() {
       </div>
 
       {/* flowers */}
-      <div className='hidden md:block absolute bottom-0 left-0 lg:-left-10 2xl:left-3 2xl:rotate-6'>
+      <div className='hidden md:block absolute bottom-0 left-0 lg:-left-10 2xl:-left-22'>
         <div className='aspect-[310/549] h-[549px] w-[110px] lg:w-[310px] relative'>
           <Image src={flowerLeft} alt='flower' fill className='object-fill' />
         </div>
       </div>
-      <div className='hidden md:block absolute bottom-0 right-0 lg:-right-10 2xl:right-0 2xl:-rotate-6'>
+      <div className='hidden md:block absolute bottom-0 right-0 lg:-right-10 2xl:-right-28'>
         <div className='aspect-[414/612] h-[612px] w-[214px] lg:w-[414px] relative'>
           <Image src={flowerRight} alt='flower' fill className='object-fill' />
         </div>
