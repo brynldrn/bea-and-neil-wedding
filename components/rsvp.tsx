d869626c092bf4cc2b25bb5680c8c8f174a5dfa2
Bryan Aldrin Quinalayo <bryandummy01@gmail.com>
2025-05-08 19:54:42 +0800

    feat: append new field and structure update

--- components/rsvp.tsx
+++ components/rsvp.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { SubmitHandler, useFieldArray, useForm } from "react-hook-form"
-import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
 import { Label } from "./ui/label"
 import { LoaderCircle, PlusIcon, X } from "lucide-react"
 import Image from "next/image"
 import flowerExtra from '@/public/icons/flower-extra.svg'
 import flowerExtra2 from '@/public/icons/flower-extra-2.svg'
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { writeToSheets } from "@/app/actions"
 import { toast } from "sonner"
 import { z } from "zod"
@@ -21,13 +21,14 @@ export default function Rsvp() {
   const formSchema = z.object({
     names: z.array(z.object({
       name: z.string().nonempty('Please enter a name here')
-    }))
+    })),
+    contactNumber: z.string().nonempty('Please enter a contact number here'),
   })
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   })
-  const { control, handleSubmit, register, reset, formState: { errors } } = form || {}
+  const { control, handleSubmit, register, reset, formState: { errors }, clearErrors } = form || {}
   const { fields, append, remove } = useFieldArray({
     control,
     name: "names"
@@ -64,6 +65,8 @@ export default function Rsvp() {
       shouldFocus: false
     })
   }, [])
+
+  useEffect(() => clearErrors(), [isAttending])
 
   return (
     <section id="rsvp" className="p-8 bg-brown-4 flex flex-col gap-16 overflow-hidden lg:py-24">
@@ -152,34 +155,82 @@ export default function Rsvp() {
                   <p className="font-sans text-base text-cream font-bold">{isAttending === 'yes' || isAttending === undefined ? 'Enter your name and the names of those attending with you.' : 'Wish you could be there! Please enter the names of those who will not be attending the event.'} <span className="text-brown-6">*</span></p>
                   {fields?.map((field, index) => {
                     const { message, name } = errors?.names?.[index] || {}
+                    const { message: phoneNumberMessage } = errors?.contactNumber || {}
 
                     return (
-                      <FormField name={`names.${index}.name`} key={field.id} render={() => (
-                        <FormItem>
-                          <FormControl>
-                            <div className="flex items-center relative">
-                              <input {...register(`names.${index}.name`)} type="text" placeholder="Enter yout name here" className={cn('placeholder:font-serif placeholder:italic placeholder:text-base placeholder:opacity-50 placeholder:text-cream border-1 py-2 text-cream font-serif italic w-full px-4 rounded-sm', {
-                                'border-brown-6': name,
-                                'border-brown-3': !name
-                              })} />
+                      <Fragment key={field.id}>
+                        <div className="grid grid-cols-2 gap-4 w-full">
+                          <div className={cn('w-full col-start-1', {
+                            'col-end-1': index === 0 && isAttending === 'yes',
+                            'col-span-2': index !== 0 || isAttending !== 'yes',
+                          })}>
+                            <FormField name={`names.${index}.name`} render={() => (
+                              <FormItem>
+                                <FormLabel className="gap-0 font-sans text-sm">
+                                  <span className="text-cream">{index === 0 && isAttending === 'yes' ? 'Name of contact person' : 'Name'}</span>
+                                  <span className="text-brown-6">*</span>
+                                </FormLabel>
+                                <FormControl>
+                                  <div className="flex items-center relative">
+                                    <input {...register(`names.${index}.name`)} type="text" className={cn('placeholder:font-serif placeholder:italic placeholder:text-base placeholder:opacity-50 placeholder:text-cream border-1 py-2 text-cream font-serif italic w-full px-4 rounded-sm', {
+                                      'border-brown-6': name,
+                                      'border-brown-3': !name
+                                    })} />
 
-                              <div className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center gap-1">
-                                {name && (
-                                  <div className="bg-brown-6 rounded-full size-5 flex items-center justify-center relative">
-                                    <Image src={exclamationMark} fill className="object-fill scale-50" alt="warning" />
+                                    <div className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center gap-1">
+                                      {name && (
+                                        <div className="bg-brown-6 rounded-full size-5 flex items-center justify-center relative">
+                                          <Image src={exclamationMark} fill className="object-fill scale-50" alt="warning" />
+                                        </div>
+                                      )}
+                                      {index !== 0 && (
+                                        <button className="bg-white rounded-full size-5 text-center flex items-center justify-center" type="button" onClick={() => remove(index)}>
+                                          <X className="size-3" color="brown" strokeWidth={3} />
+                                        </button>
+                                      )}
+                                    </div>
                                   </div>
-                                )}
-                                {index !== 0 && (
-                                  <button className="bg-white rounded-full size-5 text-center flex items-center justify-center" type="button" onClick={() => remove(index)}>
-                                    <X className="size-3" color="brown" strokeWidth={3} />
-                                  </button>
-                                )}
-                              </div>
+                                </FormControl>
+                                <FormMessage className="text-brown-6">{message}</FormMessage>
+                              </FormItem>
+                            )} />
+                          </div>
+                          {index === 0 && isAttending === 'yes' && (
+                            <div className="col-start-2">
+                              <FormField name='contactNumber' key='contactNumber' render={() => (
+                                <FormItem>
+                                  <FormLabel className="gap-0 font-sans text-sm">
+                                    <span className="text-cream">Phone Number</span>
+                                    <span className="text-brown-6">*</span>
+                                  </FormLabel>
+                                  <FormControl>
+                                    <div className="flex items-center relative">
+                                      <input {...register('contactNumber')} type="text" className={cn('placeholder:font-serif placeholder:italic placeholder:text-base placeholder:opacity-50 placeholder:text-cream border-1 py-2 text-cream font-serif italic w-full px-4 rounded-sm', {
+                                        'border-brown-6': phoneNumberMessage,
+                                        'border-brown-3': !phoneNumberMessage
+                                      })} />
+
+                                      <div className="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center gap-1">
+                                        {phoneNumberMessage && (
+                                          <div className="bg-brown-6 rounded-full size-5 flex items-center justify-center relative">
+                                            <Image src={exclamationMark} fill className="object-fill scale-50" alt="warning" />
+                                          </div>
+                                        )}
+                                      </div>
+                                    </div>
+                                  </FormControl>
+                                  <FormMessage className="text-brown-6">{phoneNumberMessage}</FormMessage>
+                                </FormItem>
+                              )} />
                             </div>
-                          </FormControl>
-                          <FormMessage className="text-brown-6">{message}</FormMessage>
-                        </FormItem>
-                      )} />
+                          )}
+                        </div>
+
+                        {/* divider */}
+                        {fields?.length > 1 && index === 0 && isAttending === 'yes' && (
+                          <div className="w-full h-[1px] bg-brown-7" />
+                        )}
+                      </Fragment>
                     )
                   })}
                   {fields?.length <= 15 && (
